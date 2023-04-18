9f446adfc513cabfe873cd2c1d31377ae1cebd1d
Ardusa <ardusa05@gmail.com>
2023-04-18 10:21:05 -0400

    added password

--- gui.java
+++ gui.java
@@ -3,22 +3,45 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.PasswordAuthentication;
 
 import javax.swing.*;
 
 class GUI extends JFrame implements ActionListener {
+    public static ActionListener actionListener;
     public static JFrame frame;
+    public static GUI gui = new GUI();
+    public static PasswordAuthentication passwordAuthentication;
+    String passwordCheck = "";
+
+    public static String userName;
+    public static char password[];
+
+    JButton button = new JButton("Show Picture");
+    JMenuBar menuBar = new JMenuBar();
+
+    JMenu menu1 = new JMenu("Options");
+    JMenu menu2 = new JMenu("File");
+
+    JMenuItem preferences = new JMenuItem("Preferences");
+    JMenuItem quit = new JMenuItem("Quit");
+    JMenuItem minimize = new JMenuItem("Minimize");
+    JMenuItem maximize = new JMenuItem("Maximize");
+
+    JMenuItem load = new JMenuItem("Load");
+    JMenuItem save = new JMenuItem("Save");
+    JMenuItem saveAs = new JMenuItem("Save As");
+    JMenuItem saveAll = new JMenuItem("Save All");
+    JPasswordField passwordField = new JPasswordField(null);
 
     public static void main(String args[]) {
-        new GUI();
+        // gui = new GUI();
     }
 
     public GUI() {
         frame = this;
         this.setAlwaysOnTop(true);
         this.setName("Picture thing");
-        JButton button = new JButton("Show Picture");
-        JMenuBar menuBar = new JMenuBar();
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -29,48 +52,48 @@ class GUI extends JFrame implements ActionListener {
         button.addActionListener(this);
         button.setActionCommand("showPic");
 
-        JMenu menu1 = new JMenu("Options");
-        JMenu menu2 = new JMenu("File");
 
-        JMenuItem preferences = new JMenuItem("Preferences");
         preferences.addActionListener(this);
         preferences.setActionCommand("options.preferences");
         menu1.add(preferences);
 
-        JMenuItem quit = new JMenuItem("Quit");
         quit.addActionListener(this);
         quit.setActionCommand("options.quit");
         menu1.add(quit);
 
-        JMenuItem minimize = new JMenuItem("Minimize");
         minimize.addActionListener(this);
         minimize.setActionCommand("options.minimize");
         menu1.add(minimize);
 
-        JMenuItem maximize = new JMenuItem("Maximize");
         maximize.addActionListener(this);
         maximize.setActionCommand("options.maximize");
         menu1.add(maximize);
 
-        JMenuItem load = new JMenuItem("Load");
         load.addActionListener(this);
         load.setActionCommand("file.load");
         menu2.add(load);
 
-        JMenuItem save = new JMenuItem("Save");
         save.addActionListener(this);
         save.setActionCommand("file.save");
         menu2.add(save);
 
-        JMenuItem saveAs = new JMenuItem("Save As");
         saveAs.addActionListener(this);
         saveAs.setActionCommand("file.save_as");
         menu2.add(saveAs);
 
-        JMenuItem saveAll = new JMenuItem("Save All");
         saveAll.addActionListener(this);
         saveAll.setActionCommand("file.save_all");
         menu2.add(saveAll);
+
+        passwordField.addActionListener(this);
+        passwordField.setActionCommand("showPic");
+        userName = "Ankur";
+        password = "password".toCharArray();
+        passwordAuthentication = new PasswordAuthentication(userName, password);
+
+        for (int x = 0; x < passwordAuthentication.getPassword().length; x++) {
+            passwordCheck += passwordAuthentication.getPassword()[x];
+        }
 
         menuBar.add(menu1);
         menuBar.add(menu2);
@@ -78,18 +101,27 @@ class GUI extends JFrame implements ActionListener {
         this.getContentPane().setLayout(new BorderLayout());
         this.getContentPane().add(menuBar, BorderLayout.NORTH);
         this.getContentPane().add(button, BorderLayout.CENTER);
+        this.getContentPane().add(passwordField, BorderLayout.SOUTH);
 
         this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println(e.getActionCommand());
-        if (e.getActionCommand() == "showPic") {
-            System.out.println("showPic");
-            // frame.add()
-        }
+        // System.out.println(e.getActionCommand());
+
         switch (e.getActionCommand()) {
+            case "showPic":
+                String pass = "";
+
+                for (int x = 0; x < passwordField.getPassword().length; x++) {
+                    pass += passwordField.getPassword()[x];
+                }
+
+                if (pass.equals(passwordCheck)) {
+                    System.out.println("showPic");
+                }
+                break;
             case "options.preferences":
                 System.out.println("preferences");
                 break;
